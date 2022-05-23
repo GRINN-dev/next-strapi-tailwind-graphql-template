c92007376594a5af96962f074a185e2b685e8f5b
Clémence Pirault <78152614+clepirault@users.noreply.github.com>
2022-05-23 11:18:28 +0200

    🔧  add graphql plugin to strapi

---
+++ strapi/config/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+  /* upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        host: env("AWS_HOST"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+    },
+  }, */
+  graphql: {
+    enabled: true,
+    config: {
+      defaultLimit: 100,
+      maxLimit: 200,
+    },
+  },
+});
