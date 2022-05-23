4d30826d05ac78969467412c7635fc05908e1510
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2022-05-23 14:59:20 +0200

    create db, create .env, config database.js and admin.js

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: `crypto.randomBytes(16).toString('base64')`,
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: `crypto.randomBytes(16).toString('base64')`,
   },
 });
