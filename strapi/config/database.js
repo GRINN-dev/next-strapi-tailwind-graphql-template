c90045ea1aaf993a96db20a395db40d8d2ccba97
Clémence Pirault <78152614+clepirault@users.noreply.github.com>
2022-05-24 10:58:50 +0200

    🐛  fix config database.js file

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi_template"),
-      user: env("DATABASE_USERNAME", "maraval"),
-      password: env("DATABASE_PASSWORD", "N2normandie"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
