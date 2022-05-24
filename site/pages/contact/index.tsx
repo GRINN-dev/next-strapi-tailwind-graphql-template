5d0eef00d31879c3ffeaa3238b5b2d2990292c86
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2022-05-24 12:37:22 +0200

    page contact, query team

--- site/pages/contact/index.tsx
+++ site/pages/contact/index.tsx
@@ -1,8 +1,23 @@
-import { NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
+import { TeamDocument, TeamQuery } from '../../graphql';
 import { client } from '../../lib/apolloClient';
 
-const Contact: NextPage = () => {
-  return <div>yo</div>;
+const Contact: NextPage<TeamQuery> = ({ teams }) => {
+  return (
+    <div>
+      <h1>{teams?.data[0]?.attributes?.teamTitle}</h1>
+      <p>{teams?.data[0]?.attributes?.teamParagraph}</p>
+    </div>
+  );
 };
 
 export default Contact;
+
+export const getStaticProps: GetStaticProps<TeamQuery> = async ({}) => {
+  const { data, error } = await client.query<TeamQuery>({
+    query: TeamDocument,
+  });
+  return {
+    props: data,
+  };
+};
