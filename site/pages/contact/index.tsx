5bd3d6d63c7884c680e4a4c2b55333d4836c28a0
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2022-05-24 14:19:56 +0200

    import team query for meta and team data

--- site/pages/contact/index.tsx
+++ site/pages/contact/index.tsx
@@ -1,12 +1,23 @@
 import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
 import { TeamDocument, TeamQuery } from '../../graphql';
 import { client } from '../../lib/apolloClient';
 
 const Contact: NextPage<TeamQuery> = ({ teams }) => {
   return (
-    <div>
-      <h1>{teams?.data[0]?.attributes?.teamTitle}</h1>
-      <p>{teams?.data[0]?.attributes?.teamParagraph}</p>
+    <div className='flex flex-col space-y-2'>
+      <Head>
+        <title>Our team</title>
+        <meta
+          name={teams?.data[0]?.attributes?.teamMeta?.metaName?.toString()}
+          property={teams?.data[0]?.attributes?.teamMeta?.metaProperty?.toString()}
+          content={teams?.data[0]?.attributes?.teamMeta?.metaContent?.toString()}
+        />
+      </Head>
+      <h1 className='text-3xl text'>{teams?.data[0]?.attributes?.teamTitle}</h1>
+      <p className='text-gray-500'>
+        {teams?.data[0]?.attributes?.teamParagraph}
+      </p>
     </div>
   );
 };
