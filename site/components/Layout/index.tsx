6a1fe5893e0430023867f6f344e8166de9cd9b9b
Clémence Pirault <78152614+clepirault@users.noreply.github.com>
2022-05-25 16:37:02 +0200

    ♻️ refacto components folder

--- site/components/Layout/index.tsx
+++ site/components/Layout/index.tsx
@@ -1,15 +1,2 @@
-import React, { FC } from "react";
-import { Footer } from "./Footer";
-import { Header } from "./Header";
-
-export const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <>
-      <Header />
-      <main className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
-        {children}
-      </main>
-      <Footer />
-    </>
-  );
-};
+export * from "./types";
+export * from "./Layout";
--- site/lib/fakeDatas.tsx
+++ site/lib/fakeDatas.tsx
@@ -1,12 +1,7 @@
-import { Meta, Story } from "@storybook/react";
-import { Footer, FooterProps } from "./";
+import { FooterProps, HeaderProps } from "../components";
+import { NewspaperIcon, OfficeBuildingIcon } from "@heroicons/react/outline";
 
-export default {
-  title: "Component/Footer",
-  component: Footer,
-} as Meta;
-
-const FooterPropsExample: FooterProps = {
+export const FooterPropsExample: FooterProps = {
   navigation: [
     {
       title: "L'association",
@@ -102,8 +97,56 @@ const FooterPropsExample: FooterProps = {
   },
 };
 
-const Template: Story<FooterProps> = () => {
-  return <Footer {...FooterPropsExample} />;
+export const HeaderPropsExample: HeaderProps = {
+  header: [
+    { label: "Notre histoire", href: "" },
+    { label: "Qui sommes nous", href: "" },
+    { label: "L'équipe", href: "" },
+  ],
+  seeMore: {
+    subHeader: [
+      {
+        label: "La presse en parle",
+        href: "",
+        subHeader: [
+          {
+            label: "Le Point - 25/05/2022",
+            href: "",
+            icon: (
+              <NewspaperIcon
+                className="flex-shrink-0 w-6 h-6 text-gray-400"
+                aria-hidden="true"
+              />
+            ),
+          },
+        ],
+      },
+      {
+        label: "Nos projets",
+        href: "",
+        subHeader: [
+          {
+            label: "Projet Patrimoine",
+            href: "",
+            icon: (
+              <OfficeBuildingIcon
+                className="flex-shrink-0 w-6 h-6 text-gray-400"
+                aria-hidden="true"
+              />
+            ),
+          },
+        ],
+      },
+    ],
+    blog: [
+      {
+        title: "S'engager dans l'associatif",
+        href: "",
+        description:
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt enim atque, accusamus veniam laboriosam, omnis exercitationem explicabo ipsa voluptate voluptatem nostrum! Consequuntur reiciendis quidem neque quae magni provident laudantium error.",
+        imageUrl:
+          "https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80",
+      },
+    ],
+  },
 };
-
-export const FooterExample = Template.bind({});
