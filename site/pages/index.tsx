6a877cc826886a9c69ad00a55de95e42bb3dba4f
Clémence Pirault <78152614+clepirault@users.noreply.github.com>
2022-05-25 16:37:26 +0200

    ✨ add Layout

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -2,10 +2,11 @@ import type { GetStaticProps, NextPage } from "next";
 import { Layout } from "../components";
 import { TestDocument, TestQuery } from "../graphql";
 import { client } from "../lib/apolloClient";
+import { FooterPropsExample, HeaderPropsExample } from "../lib/fakeDatas";
 
 const Home: NextPage<TestQuery> = ({ tests }) => {
   return (
-    <Layout>
+    <Layout header={HeaderPropsExample} footer={FooterPropsExample}>
       <h1 className="text-2xl">Hello world</h1>
       <p>{tests?.data[0]?.attributes?.title}</p>
       <div className="h-96" />
