476a58c8e94ea9ea28aa60e29a247fcb74373ac2
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2022-06-09 09:51:26 +0200

    ✨ component statistics, story

--- site/components/Statistics/Statistics.tsx
+++ site/components/Statistics/Statistics.tsx
@@ -2,18 +2,11 @@ import { FC } from 'react';
 
 export interface StatisticsProps {
   title?: string;
-  descriptionTitle?: string;
-  descriptionDetail?: string;
+  content?: { descriptionTitle?: string; descriptionDetail?: string }[];
   image?: any;
 }
 
-export const Statistics: FC<StatisticsProps> = ({
-  title,
-
-  descriptionTitle,
-  descriptionDetail,
-  image,
-}) => {
+export const Statistics: FC<StatisticsProps> = ({ title, content, image }) => {
   return (
     <div>
       <h1 className='text-3xl font-extrabold text-gray-900 sm:text-4xl'>
@@ -26,14 +19,23 @@ export const Statistics: FC<StatisticsProps> = ({
           <div className='relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
             <div className='max-w-4xl mx-auto'>
               <dl className='bg-white rounded-lg shadow-lg sm:grid sm:grid-cols-3'>
-                <div className='flex flex-col p-6 text-center border-b border-gray-100 sm:border-0 sm:border-r'>
-                  <dt className='order-2 mt-2 text-lg font-medium leading-6 text-gray-500'>
-                    {descriptionDetail}
-                  </dt>
-                  <dd className='order-1 text-5xl font-extrabold text-indigo-600'>
-                    {descriptionTitle}
-                  </dd>
-                </div>
+                {content?.map((c, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className='flex flex-col justify-center p-6 text-center border-b border-gray-100 sm:border-0 sm:border-r'
+                    >
+                      <div>
+                        <dd className='text-5xl font-extrabold text-indigo-600'>
+                          {c.descriptionTitle}
+                        </dd>
+                        <dt className='mt-2 text-lg font-medium leading-6 text-gray-500 '>
+                          {c.descriptionDetail}
+                        </dt>
+                      </div>
+                    </div>
+                  );
+                })}
               </dl>
             </div>
           </div>
